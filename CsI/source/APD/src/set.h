3acedf7f8646724af23a1e8cac864c2ec1dd0c42
hanX3 <hanxingchi@gmail.com>
2022-05-29 10:39:48 +0800

    add fit method

---
+++ CsI/source/APD/src/set.h
@@ -0,0 +1,54 @@
+//#define DEBUGWAVEANALYSIS
+//#define DEBUGDRAWOPTION
+
+#define MAXLENGTH 32768
+#define POLARITY -1
+
+// CFD Parameters
+#define CFDPARPRITRIGGER    200
+#define CFDPARSMOOTHLENGTH  60
+#define CFDPARRISELENGTH    60
+#define CFDPARTHRESHOLD     30
+
+// Trapezoid Parameters
+#define TRAPPARDECAYLENGTH  6000
+#define TRAPPARRISELENGTH   2000
+#define TRAPPARFLATTOP      400
+#define TRAPPARPRITRIGGER   200
+#define TRAPPARPEAKDELAY    0
+#define TRAPPARPEAKMEAN     0
+
+//
+#define PILEUPMAX 2
+
+// PID
+#define Q1START  500
+#define Q1STOP   800
+#define Q2START  2000
+#define Q2STOP   4000
+
+#define FITSTART 0
+#define FITSTOP  4000
+
+#define BASELINE            0.
+#define BASELINEMIN         -10.
+#define BASELINEMAX         10.
+#define T0                  400.
+#define T0MIN               200.
+#define T0MAX               600.
+#define TAURC               5000.
+#define TAURCMIN            4000.
+#define TAURCMAX            8000.
+#define TAUFAST             240.
+#define TAUFASTMIN          50.
+#define TAUFASTMAX          500.
+#define TAUSLOW             4000.
+#define TAUSLOWMIN          800.
+#define TAUSLOWMAX          40000.
+#define AMFAST              300.
+#define AMFASTMIN           10.
+#define AMFASTMAX           3000.
+#define AMSLOW              300.
+#define AMSLOWMIN           10.
+#define AMSLOWMAX           3000.
+
