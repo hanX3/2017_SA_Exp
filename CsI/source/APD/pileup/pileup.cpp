94bbbfbb96f0ddbc85f5fa8c2e4f2d902fd345ef
hanX3 <hanxingchi@gmail.com>
2022-06-16 11:05:24 +0800

    update pileup

--- CsI/source/APD/pileup/pileup.cpp
+++ CsI/source/APD/pileup/pileup.cpp
@@ -1,7 +1,16 @@
 #define MAXTRIGGERNUM 2
 #define MAXLENGTH 32768
 
+//
+Double_t Fun1s(Double_t *i, Double_t *p);
+// Double_t Fun2s(Double_t *i, Double_t *p);
+// Double_t Fun3s(Double_t *i, Double_t *p);
 
+Double_t FunA(Double_t *i, Double_t *p);
+Double_t FunB(Double_t *i, Double_t *p);
+Double_t FunC(Double_t *i, Double_t *p);
+
+//
 void pileup(Long64_t n)
 {
   TFile *fi1 = TFile::Open("../../../../../data/CsI/source/run_R0040/run_R0040_000000000_000001000_ana.root");
@@ -67,10 +76,110 @@ void pileup(Long64_t n)
   h1->Draw();
 
   //expo fit
-  //todo
+  // TF1 *tfa = new TF1("tfa", FunA, 0, 10000, 5);
+  // tfa->SetParameter(0, 0);
+  // tfa->SetParameter(1, 600);
+  // tfa->SetParameter(2, 400);
+  // tfa->SetParameter(3, 400);
+  // tfa->SetParameter(4, 1000);
+  // h1->Fit("tfa", "RW");
 
+  // TF1 *tfb = new TF1("tfb", FunB, 0, 10000, 4);
+  // tfb->SetParameter(0, 0);
+  // tfb->SetParameter(1, 600);
+  // tfb->SetParameter(2, 400);
+  // tfb->SetParameter(3, 400);
+  // h1->Fit("tfb", "RW");
 
+  TF1 *tfc = new TF1("tfc", FunC, 0, 10000, 2);
+  tfc->SetParameter(0, 0);
+  tfc->SetParameter(1, 600);
+  h1->Fit("tfc", "RW");
 
 
   return ;
 }
+
+//
+Double_t FunA(Double_t *i, Double_t *p)
+{
+  Double_t s = p[0];
+  Double_t x = i[0] - p[1];
+  Double_t e = exp(-x/p[2]);
+
+  if(x<0) return s;
+  else{
+    s += p[4]*(1-exp(-x/p[3]))*e;
+    return s;
+  }
+}
+
+//
+Double_t FunB(Double_t *i, Double_t *p)
+{
+  Double_t s = p[0];
+  Double_t x = i[0] - p[1];
+
+  if(x<0) return s;
+  else{
+    s += (p[1] + p[2]*x)*exp(-x/p[3]);
+    return s;
+  }
+}
+
+//
+Double_t FunC(Double_t *i, Double_t *p)
+{
+  Double_t s = p[0];
+  Double_t x = i[0] - p[1];
+
+  if(x<0) return s;
+  else{
+    s += exp(-(log(x*x)));
+    return s;
+  }
+}
+
+//
+// p[0]: baseline
+// p[1]: t0 (in points)
+// p[2]: tau rc (in points)
+// p[3]: tau fast (in points)
+// p[4]: tau slow (in points)
+// p[5]: am fast
+// p[6]: am slow
+Double_t Fun1s(Double_t *i, Double_t *p)
+{
+  Double_t s = p[0];
+  Double_t x = i[0] - p[1];
+  Double_t e = exp(-x/p[2]);
+
+  if(x<0) return s;
+  else{
+    s += p[5]*(1-exp(-x/p[3]))*e; 
+    s += p[6]*(1-exp(-x/p[4]))*e;
+  return s;
+  }
+}
+
+// //
+// // p[0]: baseline
+// // p[1]: 0 --> t0 (in points)
+// // p[2]: tau rc (in points)
+// // p[3]: 0 --> tau fast (in points)
+// // p[4]: 0 --> tau slow (in points)
+// // p[5]: 0 --> am fast
+// // p[6]: 0 --> am slow
+// Double_t Fun2s(Double_t *i, Double_t *p)
+// {
+//   Double_t s = p[0];
+//   Double_t x = i[0] - p[1];
+//   Double_t e = exp(-x/p[2]);
+
+//   if(x<0) return s;
+//   else if(){
+//     s += p[5]*(1-exp(-x/p[3]))*e; 
+//     s += p[6]*(1-exp(-x/p[4]))*e;
+//   return s;
+//   }
+// }
